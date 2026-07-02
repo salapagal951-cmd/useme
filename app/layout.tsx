41f4dbf76bd821765ddc2fb521c7f346f80b2618
Sonu <salapagal951@gmail.com>
2026-07-02 23:14:21 +0530

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        {children}
-        <Analytics />
-      </body>
+     <body className="min-h-full flex flex-col">
+  {children}
+  <Analytics />
+  <SpeedInsights />
+</body>
     </html>
   );
 }
